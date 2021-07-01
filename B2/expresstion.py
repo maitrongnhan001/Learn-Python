6d40cb7118d4909c397cdab0b423ff9c9e931e4d
TrongNhan <trongnhan@TrongNhans-MacBook-Pro.local>
2021-07-01 13:35:30 +0700

    Learn expresstion

---
+++ B2/expresstion.py
@@ -0,0 +1,5 @@
+length = 5
+breath = 2
+area = length * breath
+print("Area is: ", area)
+print("Perimeter is: ", 2 * (length + breath))
---
+++ B2/var.py
@@ -0,0 +1,8 @@
+i = 5
+print(i)
+i = i + 1
+print(i)
+#dau gach cheo nguoc giup chuyen tu long line sang single lines
+s = '''This is a multi-line string.\
+This is the second line'''
+print(s);
