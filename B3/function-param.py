9e77c5a12e3ed7bf51affd2ce8cf17f2a02982d6
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-07-03 16:13:29 +0700

    learn funtion paramester

---
+++ B3/function-param.py
@@ -0,0 +1,13 @@
+def print_max (a, b) :
+    if( a > b ) :
+        print(a, " is maximum")
+    elif ( a == b) :
+        print(a, " is equal to ", b)
+    else :
+        print(b, " is maximum")
+
+print_max(3, 7)
+
+x = 3
+y = 7
+print_max(x, y)
