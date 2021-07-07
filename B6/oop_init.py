b8a0aa5629b29059b7295515bb875b77b33faf25
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-07-07 12:21:12 +0700

    learn oop variables

---
+++ B6/oop_init.py
@@ -0,0 +1,9 @@
+class Preson :
+    def __init__(self, name) :
+        self.name = name
+    
+    def say_hi(seft) :
+        print('hello, my name is', seft.name)
+
+P = Preson('Mai Trong Nhan')
+P.say_hi();
